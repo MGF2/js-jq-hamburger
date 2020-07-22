0cae92628dac2799604fd0329c54df6089af9784
Unknown <fioremartina@hotmail.com>
2020-07-22 17:39:04 +0200

    con active+before

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ $('.hamburger-menu > a').click(function(){
   $('.hamburger-menu').removeClass("active");
 
 });
+
+//Before
+// $('.header-right > a').click(function(){
+//
+//   $('.hamburger-menu').show(200);
+//
+// });
+//
+// $('.hamburger-menu > a').click(function(){
+//
+//   $('.hamburger-menu').hide(200);
+//
+// });
